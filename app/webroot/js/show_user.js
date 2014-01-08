01a85a43dae7662708ed5db7831ba1c5707b1ec6
uclars <postpsych@yahoo.co.jp>
2014-01-08 12:34:43 -0500

    users show user topic delete Topic controller function 8

--- app/webroot/js/show_user.js
+++ app/webroot/js/show_user.js
@@ -10,7 +10,7 @@ $(function(){
  						+"<p id='delete_confirm' class='delete_confirm'>&nbsp&nbspdelete&nbsp&nbsp</p> "
 						+" <p id='delete_cancel' class='delete_cancel'>&nbsp&nbspcancel&nbsp&nbsp</p>"
 			                   +"</div>";
-			$(this).parent(".span2").before(confirm_link).animate({}, 400); //add new div to show confirm link
+			$(this).parent(".span2").insertBefore(confirm_link).animate({}, 400); //add new div to show confirm link
 
 /*
 		$(this).parents("#topic_box").animate({width: 'hide', height: 'hide', opacity: 'hide'}, 'slow', function () {
