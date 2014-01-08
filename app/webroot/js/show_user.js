41549bfeb2f6e827bca8ab9906edc25f6a1b7800
uclars <postpsych@yahoo.co.jp>
2014-01-08 10:35:19 -0500

    users show user topic delete 10

--- app/webroot/js/show_user.js
+++ app/webroot/js/show_user.js
@@ -8,7 +8,7 @@ $(function(){
 			$(this).children(".span6").attr("style","display:none");
 
 			//display confirm link
-			var confirm_link = "<div class='span8' style='text-align:center;'>" 
+			var confirm_link = "<div class='span8' style='text-align:center; margin-bottom:10px;'>" 
 					   +"<div id='areyousure' style='color:#f63; font-weight:bold; margin-top:10px; text-align:center;'> Are you sure to delete this topic?</div>"
  					   +"<p class='delete_confirm'>[delete]</p> "
 					   +" <p class='delete_confirm'>[cancel]</p>"
