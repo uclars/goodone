fe25d36f8156320ad25a5cd35d307fff83143da7
uclars <postpsych@yahoo.co.jp>
2014-01-08 13:03:16 -0500

    users show user topic delete Topic controller function 23

--- app/webroot/js/show_user.js
+++ app/webroot/js/show_user.js
@@ -2,7 +2,10 @@
 // delete a topoic
 $(function(){
 	$("#topic_delete").live('click',function(e){
-
+			//disappear the delete topic link
+			$(".span2, #topic_delete",this).attr("style","display:none");
+			$(this).children(".span2").attr("style","margin-bottom:13px");
+			//$(this).children(".span2").show();
 
 			//display confirm link
 			var confirm_link = "<div id='confirm_link' class='span8' style='text-align:center; margin-bottom:10px;'>" 
@@ -10,8 +13,8 @@ $(function(){
  						+"<p id='delete_confirm' class='delete_confirm'>&nbsp&nbspdelete&nbsp&nbsp</p> "
 						+" <p id='delete_cancel' class='delete_cancel'>&nbsp&nbspcancel&nbsp&nbsp</p>"
 			                   +"</div>";
-			//$(this).parent(".span2").before(confirm_link); //add new div to show confirm link
-			$(this, confirm_link).hide().insertBefore(".span2").fadeIn(10000); //add new div to show confirm link
+			$(this).parent(".span2").before(confirm_link); //add new div to show confirm link
+			//$(confirm_link).hide().insertBefore(".span2",this).fadeIn(10000); //add new div to show confirm link
 
 
 /*
