35b27748f5db3baa3475b038e7ea376655e0520f
uclars <postpsych@yahoo.co.jp>
2014-01-09 02:29:40 -0500

    show_user.js delete topic delete/cancel function 24

--- app/webroot/js/show_user.js
+++ app/webroot/js/show_user.js
@@ -13,7 +13,7 @@ $(function(){
 			                   +"</div>";
 			//$(this).parent(".span2").before(confirm_link); //add new div to show confirm link
 			//$(confirm_link).hide().insertBefore(".span2",this).fadeIn(10000); //add new div to show confirm link
-			 $(confirm_link).hide().fadeIn('slow').insertBefore((this).closest(".span2"));
+			 $(confirm_link).hide().fadeIn('slow').insertBefore($(this).closest(".span2"));
 
 /*
 		$(this).parents("#topic_box").animate({width: 'hide', height: 'hide', opacity: 'hide'}, 'slow', function () {
