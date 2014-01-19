d0a30c373d10e4baacc6b31d4bd9883face71973
uclars <postpsych@yahoo.co.jp>
2014-01-19 08:07:31 -0500

    jquery image uploda 66

--- app/webroot/js/utility.js
+++ app/webroot/js/utility.js
@@ -246,8 +246,10 @@ $("#uploadedfile").live("change", function(){
 		//hide the loading image when images are shown
 		$('#loadingimage').hide();
 		//show the images
-var url;
-url=res.split("|");
+var response=res.split("|");
+var url_number=res.split("|");
+var url=response[1].substr(0,(url_number));
+
 		$("#preview").text(url);
 console.log(url);
 
