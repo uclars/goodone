432fc3613feb5f0859ede85c158b7d355c2c8c99
uclars <postpsych@yahoo.co.jp>
2014-01-19 07:20:54 -0500

    jquery image uploda 49

--- app/webroot/js/utility.js
+++ app/webroot/js/utility.js
@@ -247,7 +247,7 @@ $("#uploadedfile").live("change", function(){
 		$('#loadingimage').hide();
 		//show the images
 //		$("#display_img").html(res);
-$('#display_img').empty().html( '<img src="' + res + '">' );
+$('#display_img').html( '<img src="' + res + '">' );
 return false;  
 	}); 
 });
