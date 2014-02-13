7170e24f14a708a5bf82b07806fbca553cfdbfc0
uclars <postpsych@yahoo.co.jp>
2014-02-13 10:23:03 -0500

    youtube 36

--- app/webroot/js/utility.js
+++ app/webroot/js/utility.js
@@ -291,6 +291,11 @@ $('#get_youtube').live('click',function(e){
 //show the images
 	//$("<a href='http://www.youtube.com/watch?v=8e_wXc0m97w' rel='nofollow' id='youtube'>Check out this video</a>").appendTo("#display_youtube").hide().fadeIn(800);
 	$("#display_youtube").append('<a href="http://www.youtube.com/watch?v=8e_wXc0m97w" rel="nofollow" id="youtube">Check out this video</a>');
+
+	$('#youtube').youtubin({
+		swfWidth:560,
+		 swfHeight:340
+	});
 });
 
 
