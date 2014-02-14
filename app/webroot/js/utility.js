9fcd4d8feec9c903d147aa9b120bcf8e01ca03ff
uclars <postpsych@yahoo.co.jp>
2014-02-14 08:13:27 -0500

    youtube thumnail 27

--- app/webroot/js/utility.js
+++ app/webroot/js/utility.js
@@ -239,10 +239,9 @@ $('#get_youtube').live('click',function(e)
 */
 
 	//Clear the images
-	$('#get_youtube').fadeOut(500, function() {
+	$('#youtube').fadeOut(500, function() {
 		$(this).html("").fadeIn(500);
 	});
-	$('#get_youtube').hide();
 
 	e.preventDefault();
 });
