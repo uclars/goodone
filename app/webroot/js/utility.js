0af6bf27e0cf89224c0484dbeada11dbe4ee695c
uclars <postpsych@yahoo.co.jp>
2014-02-21 02:21:50 -0500

    youtube upload 20

--- app/webroot/js/utility.js
+++ app/webroot/js/utility.js
@@ -337,7 +337,7 @@ var youtubeurl_content = $('#youtube_input').attr("value");
 	//$("#display_youtube").append('<a href="http://www.youtube.com/watch?v=8e_wXc0m97w" rel="nofollow" id="youtube">Check out this video</a>');
 	//$("#get_youtube").attr("src",$.jYoutube('http://www.youtube.com/watch?v=8e_wXc0m97w', 'big'));
 	$("#get_youtube").attr("src",$.jYoutube(youtubeurl_content, 'big'));
-	$("#get_youtube").attr("alt","http://www.youtube.com/watch?v=8e_wXc0m97w(__)youtube(__)youtube");
+	$("#get_youtube").attr("alt",youtubeurl_content+"(__)youtube(__)youtube");
 	$("#get_youtube").show('slow');
 });
 
