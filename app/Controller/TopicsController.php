2b4437f6705a07f9dfb8de7aaa1ab105bff31265
uclars <postpsych@yahoo.co.jp>
2014-03-03 13:40:03 -0500

    administrations check admin 6

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -1029,6 +1029,9 @@ echo "</PRE>";
 			$topicid=$tid[1];
 			$conditions = array();
 			$conditions['Topic.id'] = $tid[1];
+
+debug($treferer);
+
 			//check if the traffic comes form admin page, otherwise it comes from direct which is not correct
 			if($treferer == "http://0-0b.com/administrations"){
 				if(!$is_admin){
