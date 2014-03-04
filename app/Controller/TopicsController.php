32e18ff7a4a23620a4d2e1f109e2492e55a1e9db
uclars <postpsych@yahoo.co.jp>
2014-03-04 03:33:48 -0500

    administration add input form in View 4

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -977,7 +977,7 @@ echo "</PRE>";
 		$topic_check = $topic_array[0]['Topic']['checked'];
 
 		$contents_array = array();
-		$contents_array[0]=array($topic_id,$topic_title,$topic_category,$topic_description);
+		$contents_array[0]=array($topic_id,$topic_title,$topic_category,$topic_description,$topic_userid,$topic_check);
 		$i = $j = $k = 0;
 
 		if(!empty($topic_array[0]['Title']) AND !empty($topic_array[0]['Content']) and !empty($topic_array[0]['Comment'])){
