387f951180d3fc7d9ea79ac9b0c9c75a95fa6116
uclars <postpsych@yahoo.co.jp>
2014-03-06 09:14:19 -0500

    administartions check button bye admin 52

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -187,7 +187,7 @@ echo "</PRE>";
 
 
                 $topic_array = $this->Topic->find('all', array(
-                        'conditions' => array('Topic.id' => $newtopicid, 'Topic.hide' => 0, 'Topic.deleted' => 0),
+                        'conditions' => array('Topic.id' => '150', 'Topic.hide' => 0, 'Topic.deleted' => 0),
                         'contain' => array(
                                 'Mastercategory',
                                 'Title.title',
@@ -197,7 +197,6 @@ echo "</PRE>";
                         )
                 ));
 
-debug($newtopicid);
 debug($topic_array);
 debug($newtopicid);
 debug($this->params);
