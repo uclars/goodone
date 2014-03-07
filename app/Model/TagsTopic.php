aa0dff34843ddf2b5a2d3ade9be9e84eb559486f
uclars <postpsych@yahoo.co.jp>
2014-03-07 13:41:21 -0500

    administration tag edit 58

--- app/Model/TagsTopic.php
+++ app/Model/TagsTopic.php
@@ -14,6 +14,23 @@ class TagsTopic extends AppModel
 		),
 	);
 
+	function updateNewTagTopic($topicid, $newtagid){
+		$isTagTopic=$this->find('all',array(
+                        'conditions' => array('TagsTopic.topic_id' => $topicid, 'TagsTopic.tag_id' => $newtagid)
+                ));
+
+debug($isTagTopic);
+exit;
+
+
+                $tagdata = array();
+                $tagdata['topic_id'] = $topicid;
+                $tagdata['tag_id'] = $newtagid;
+
+                $this->TagsTopic->create();
+                $this->TagsTopic->save($tagdata);
+        }
+
 	function markDelete($topic_id,$d_tag){
 		$delte_query = "Update tags_topics Set deleted=1 Where topic_id=".$topic_id." AND tag_id=".$d_tag.";";
 		$this->query($delte_query);
