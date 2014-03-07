cf2fd873118c0fdcbcaa2336b486a8634ccf4e8c
uclars <postpsych@yahoo.co.jp>
2014-03-07 10:47:39 -0500

    administration tag edit 17

--- app/Model/TagsTopic.php
+++ app/Model/TagsTopic.php
@@ -15,10 +15,13 @@ class TagsTopic extends AppModel
 	);
 
 	function markDelete($topic_id,$d_tag){
+debug($d_tag);
 		$tagdata = array();
 		$tagdata['topic_id'] = $topic_id;
 		$tagdata['tag_id'] = $d_tag;
 		$tagdata['deleted'] = 1;
+
+debug($tagdata);
 
 		$this->create();
 		$this->save($tagdata);
