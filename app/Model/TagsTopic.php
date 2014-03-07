19f67c08ab6fdb596e625083d1a46b2cb683813c
uclars <postpsych@yahoo.co.jp>
2014-03-07 11:11:20 -0500

    administration tag edit 24

--- app/Model/TagsTopic.php
+++ app/Model/TagsTopic.php
@@ -15,7 +15,7 @@ class TagsTopic extends AppModel
 	);
 
 	function markDelete($topic_id,$d_tag){
-		$delte_query = "Update tags_topics Set 'deleted'=1 Where 'topic_id'=".$topic_id." AND 'tag_id'=".$d_tag.";";
+		$delte_query = "Update tags_topics Set deleted=1 Where topic_id=".$topic_id." AND tag_id=".$d_tag.";";
 		$this->query($delte_query);
 	}
 }
