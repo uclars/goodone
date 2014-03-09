a2f413952762dead2937b891c764714153c60c6a
uclars <postpsych@yahoo.co.jp>
2014-03-09 07:27:51 -0400

    administration create 54

--- app/Model/TagsTopic.php
+++ app/Model/TagsTopic.php
@@ -22,9 +22,6 @@ class TagsTopic extends AppModel
 			)
                 ));
 
-debug($isTagTopic);
-
-
 		$tagstopic_id=$isTagTopic[0]['TagsTopic']['id'];
                 $tagdata = array();
                 $tagdata['id'] = $tagstopic_id;
