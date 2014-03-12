fe73003113dae2f9ba4c06450e8fc7d2f5791d6f
uclars <postpsych@yahoo.co.jp>
2014-03-12 12:18:23 -0400

    create topic edit 3

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -486,7 +486,7 @@ exit;
 
 
 				//save Topic name and category to DB
-				$result = $this->Topic->save($data);
+//				$result = $this->Topic->save($data);
 				// get the last id
 				$topicid = $this->Topic->getLastInsertID();
 
@@ -496,23 +496,23 @@ exit;
 			
 				//save the first tag(category) into DB
 				$orgdatacontents=$orgcontents;
-//				$this->_update_tag($orgcontents,$orgdatacontents,$this->data,$topicid,$me);
+				$this->_update_tag($orgcontents,$orgdatacontents,$this->data,$topicid,$me);
 
 
 				// save titles to DB
 				$newtitlearray['Content']['title'] = $this->_make_newdata_title_array($datacontents);
 				if(!empty($newtitlearray['Content']['title'][0])){
-//					$this->_save_new_topic_title($newtitlearray, $data, $topicid);
+					$this->_save_new_topic_title($newtitlearray, $data, $topicid);
 				}
 				//save contents to DB
 				$newcontentarray['Content']['content'] = $this->_make_newdata_content_array($datacontents);
 				if(!empty($newcontentarray['Content']['content'][0])){
-//					$this->_save_new_topic_content($newcontentarray, $data, $topicid);
+					$this->_save_new_topic_content($newcontentarray, $data, $topicid);
 				}
 				//save comments to DB
 				$newcommentarray['Content']['comment'] = $this->_make_newdata_comment_array($datacontents);
 				if(!empty($newcommentarray['Content']['comment'][0])){
-//					$this->_save_new_topic_comment($newcommentarray, $data, $topicid);
+					$this->_save_new_topic_comment($newcommentarray, $data, $topicid);
 				}
 			}// end of if(there are values in the from)
 		}// end of else(New create or Edit)
