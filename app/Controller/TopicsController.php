38e9aa9956658262b28b8fae7ed58904a3a33583
uclars <postpsych@yahoo.co.jp>
2014-03-12 11:17:35 -0400

    administration view 5

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -51,9 +51,9 @@ class TopicsController extends AppController {
 		//Use Containable to reduce the SQL Query
 		//http://book.cakephp.org/2.0/en/core-libraries/behaviors/containable.html
 		$topic_array = $this->Topic->find('all', array(
-			'conditions' => array('Topic.id' => $topic_id, 'Topic.hide' => 0, 'Topic.deleted' => 0),
+			//'conditions' => array('Topic.id' => $topic_id, 'Topic.hide' => 0, 'Topic.deleted' => 0),
 			'conditions' => array('Topic.id' => $topic_id),
-			//'contain' => array(
+			'contain' => array(
 				'Mastercategory',
 				'Title.title',
 				'Content.content',
