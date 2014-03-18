117cd18b36d78e93e5b2750fc9598173d47520cb
uclars <postpsych@yahoo.co.jp>
2014-03-18 12:05:29 -0400

    topic create publish4

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -553,14 +553,13 @@ exit;
 
 
 debug($data);
-debug($isnewtopicinfo);
-exit;
-
 
 
 
 		/// Compare old data and new data to see if there are differences ///
 		$isnewtopicinfo=array_diff($data,$orgdata);
+debug($isnewtopicinfo);
+exit;
 		if(!empty($isnewtopicinfo)){
 			//save Topic name and category to DB
 			$this->Topic->save($data);
