d8ed7dbf8aab16fa3a29c2ffbaa32f8211671a33
uclars <postpsych@yahoo.co.jp>
2014-04-09 02:43:43 -0400

    create new topic image upload bug 3

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -506,12 +506,6 @@ exit;
 
 				// save titles to DB
 				$newtitlearray['Content']['title'] = $this->_make_newdata_title_array($datacontents);
-
-
-echo "<PRE>";
-var_dump($datacontents);
-echo "</PRE>";
-exit;
 				if(!empty($newtitlearray['Content']['title'][0])){
 					$this->_save_new_topic_title($newtitlearray, $data, $topicid);
 				}
@@ -670,6 +664,13 @@ exit;
 					}
 				}
 			}
+
+
+echo "<PRE>";
+var_dump($newtitlearray);
+echo "</PRE>";
+exit;
+
 			unset($newtitlearray[0]);  //array starts from 1
 			return $newtitlearray;
 
