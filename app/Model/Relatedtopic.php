2c6586ec1ba2ea428b6f2bbb7674c35ffea1bf58
uclars <postpsych@yahoo.co.jp>
2014-04-17 14:36:25 -0400

    related topic 24

--- app/Model/Relatedtopic.php
+++ app/Model/Relatedtopic.php
@@ -3,7 +3,7 @@ class Relatedtopic extends AppModel {
 	public $name = 'Relatedtopic';
 
 	public function update_newrelatedtopics($topicid,$new_topci_array){
-		$update_query = "update relatedtopics set first=".$new_topci_array[0]."where topicid=".$topicid.";";
+		$update_query = "update relatedtopics set first=".$new_topci_array[0]." where topicid=".$topicid.";";
 		$this->query($update_query);
 	}
 }
