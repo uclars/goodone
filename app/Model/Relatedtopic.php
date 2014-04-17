39822a730752a1c5c4bb8392ff451c24abb7d9a6
uclars <postpsych@yahoo.co.jp>
2014-04-17 15:23:19 -0400

    related topic 35

--- app/Model/Relatedtopic.php
+++ app/Model/Relatedtopic.php
@@ -10,6 +10,7 @@ class Relatedtopic extends AppModel {
 			//if there are no record on the table yet, insret new ranking
 			$insert_query = "insert into relatedtopics values('','".$topicid."','".$new_topci_array[0]."','".$new_topci_array[1]."','".$new_topci_array[2]."','".$new_topci_array[3]."','".$new_topci_array[4]."','".$new_topci_array[5]."','".$new_topci_array[6]."','".$new_topci_array[7]."','".$new_topci_array[8]."','".$new_topci_array[9]."','','');";
 			$this->query($insert_query);
+echo $insert_query;
 		}
 	}
 }
