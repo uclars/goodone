2e8f0949cd8fc4be1a4e15f3af8ecf10a358d99f
uclars <postpsych@yahoo.co.jp>
2014-04-17 15:20:03 -0400

    related topic 33

--- app/Model/Relatedtopic.php
+++ app/Model/Relatedtopic.php
@@ -8,7 +8,7 @@ class Relatedtopic extends AppModel {
 			$this->query($update_query);
 		}else{
 			//if there are no record on the table yet, insret new ranking
-			$insert_query = "insert into relatedtopics value('','".$topicid."','".$new_topci_array[0]."','".$new_topci_array[1]."','".$new_topci_array[2]."','".$new_topci_array[3]."','".$new_topci_array[4]."','".$new_topci_array[5]."','".$new_topci_array[6]."','".$new_topci_array[7]."','".$new_topci_array[8]."','".$new_topci_array[9]."','','');";
+			$insert_query = "insert into relatedtopics value('','".$topicid."','".$new_topci_array[0]."','".$new_topci_array[1]."','".$new_topci_array[2]."','".$new_topci_array[3]."','".$new_topci_array[4]."','".$new_topci_array[5]."','".$new_topci_array[6]."','".$new_topci_array[7]."','".$new_topci_array[8]."','".$new_topci_array[9]."',,);";
 			$this->query($insert_query);
 		}
 	}
