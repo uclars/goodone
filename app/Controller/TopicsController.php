cbb99e5cf5bf71360d236a5cca3624fd09138af0
uclars <postpsych@yahoo.co.jp>
2014-04-18 01:41:48 -0400

    Related topic View 5

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -206,6 +206,9 @@ echo "</PRE>";
 			return $related_topic_array;
 		}else{
 			//no update. return current list
+			foreach($related_topic_array as $related_item){
+				var_dump($related_item);
+			}
 			return $related_topic_array;
 		}
 	}
