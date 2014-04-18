7fbeed557c3ea5edcc1a3cd98c9054004886ffab
uclars <postpsych@yahoo.co.jp>
2014-04-18 03:12:41 -0400

    Related topic View 32

--- app/Model/Relatedtopic.php
+++ app/Model/Relatedtopic.php
@@ -5,7 +5,7 @@ class Relatedtopic extends AppModel {
 	public function update_newrelatedtopics($topicid,$new_topci_array,$isnew){
 		if(empty($isnew)){
 			//if there are a record, update new ranking
-			$update_query = "update relatedtopics set first=".$new_topci_array[0]."','".$new_topci_array[1]."','".$new_topci_array[2]."','".$new_topci_array[3]."','".$new_topci_array[4]."','".$new_topci_array[5]."','".$new_topci_array[6]."','".$new_topci_array[7]."','".$new_topci_array[8]."','".$new_topci_array[9]." ,modified=now() where topicid=".$topicid.";";
+			$update_query = "update relatedtopics set first=".$new_topci_array[0]."','".$new_topci_array[1]."','".$new_topci_array[2]."','".$new_topci_array[3]."','".$new_topci_array[4]."','".$new_topci_array[5]."','".$new_topci_array[6]."','".$new_topci_array[7]."','".$new_topci_array[8]."','".$new_topci_array[9]."' ,modified=now() where topicid=".$topicid.";";
 			$this->query($update_query);
 		}else{
 			//if there are no record on the table yet, insret new ranking
